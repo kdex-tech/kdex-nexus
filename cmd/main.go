668741c6639297e551aa367f0ab629528d1491d3
Raymond Augé <rayauge@doublebite.com>
2025-12-22 12:24:03 -0500

    kubebuilder create api --version v1alpha1 --kind KDexPageBinding --resource=false --controller=true

--- cmd/main.go
+++ cmd/main.go
@@ -290,6 +290,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "KDexTranslation")
 		os.Exit(1)
 	}
+	if err := (&controller.KDexPageBindingReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "KDexPageBinding")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
