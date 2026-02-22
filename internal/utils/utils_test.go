6ff9077fd86be3466ef5c9d6844955f54880dbb8
Raymond Augé <rayauge@doublebite.com>
2026-02-22 11:54:12 -0500

    tests
    Signed-off-by: Raymond Augé <rayauge@doublebite.com>

---
+++ internal/utils/utils_test.go
@@ -0,0 +1,38 @@
+package utils
+
+import (
+	"strconv"
+	"testing"
+)
+
+func TestIfElse(t *testing.T) {
+	if IfElse(true, 1, 2) != 1 {
+		t.Error("Expected 1")
+		t.Log("BUG: IfElse failed to return trueVal")
+	}
+	if IfElse(false, 1, 2) != 2 {
+		t.Error("Expected 2")
+		t.Log("BUG: IfElse failed to return elseVal")
+	}
+}
+
+func TestMapSlice(t *testing.T) {
+	input := []int{1, 2, 3}
+	output := MapSlice(input, func(v int) string {
+		return strconv.Itoa(v)
+	})
+
+	if len(output) != 3 || output[0] != "1" || output[1] != "2" || output[2] != "3" {
+		t.Errorf("Unexpected output: %v", output)
+		t.Log("BUG: MapSlice didn't correctly map all items")
+	}
+}
+
+func TestPtr(t *testing.T) {
+	val := 42
+	ptr := Ptr(val)
+	if ptr == nil || *ptr != 42 {
+		t.Errorf("Unexpected ptr: %v", ptr)
+		t.Log("BUG: Ptr returned incorrect pointer")
+	}
+}
